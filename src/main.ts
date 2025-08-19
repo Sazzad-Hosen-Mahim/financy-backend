329b4eda69d2e59ce1cfb16183ea56e8077c81bd
Sazzad-Hosen-Mahim <sajjadhosenmahim@gmail.com>
2025-08-19 18:02:04 +0600

    final commit before deploy

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5175'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
